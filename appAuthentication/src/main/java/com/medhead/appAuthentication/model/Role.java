b90f4e05a83ea727d5be69f22934915b3a9d94b6
Morgan_Facorat <facorat.morgan@gmail.com>
2023-08-10 18:08:32 +0200

    Intégration du package 'model'
    Initialisation du model User, Role, ERole

---
+++ appAuthentication/src/main/java/com/medhead/appAuthentication/model/Role.java
@@ -0,0 +1,18 @@
+package com.medhead.appAuthentication.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "roles")
+@Data
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole name;
+
+}
