cbec035d455fd4fc3e9369f55a6e7b09b6bdb4f9
Morgan_Facorat <facorat.morgan@gmail.com>
2023-08-10 19:14:54 +0200

    Intégration validateJwtToken in appGestion

--- appGestion/src/main/java/com/medhead/appGestion/controller/GestionController.java
+++ appGestion/src/main/java/com/medhead/appGestion/controller/GestionController.java
@@ -1,9 +1,107 @@
 package com.medhead.appGestion.controller;
+import com.medhead.appGestion.model.Hospital;
+import com.medhead.appGestion.model.Specialisation;
+import com.medhead.appGestion.repository.HospitalRepository;
+import com.medhead.appGestion.repository.SpecialisationRepository;
+import com.medhead.appGestion.service.HospitalService;
+import com.medhead.appGestion.service.HospitalSpecialisationService;
+import com.medhead.appGestion.service.SpecialisationService;
+import net.minidev.json.JSONArray;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 public class GestionController {
+
+    @Autowired
+    private HospitalService hospitalService;
+
+    @Autowired
+    private HospitalRepository hospitalRepository;
+
+    @Autowired
+    private SpecialisationRepository specialisationRepository;
+    @Autowired
+    private SpecialisationService specialisationService;
+    @Autowired
+    private HospitalSpecialisationService hospitalSpecialisationService;
+
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+    @PostMapping("/addHospital")
+    public ResponseEntity<?> AddHospital(@RequestBody Hospital hospital) {
+        if (hospitalRepository.existsByName(hospital.getName())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body("Error: Hospital name already exists");
+        }else {
+            hospitalService.addHospital(hospital);
+            return ResponseEntity.ok("Hospital is already added");
+        }
+
+    }
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+    @PostMapping("/addSpecialisation")
+    public ResponseEntity<?> AddSpecialisation(@RequestBody Specialisation specialisation) {
+        if (specialisationRepository.existsByName(specialisation.getName())) {
+
+            return ResponseEntity
+                    .badRequest()
+                    .body("Error: Specialisation name already exists");
+        }else {
+            specialisationService.addSpecialisation(specialisation);
+            return ResponseEntity.ok("Specialisation is already added");
+        }
+    }
+
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+    @GetMapping("/allInfo/{secialisationName}")
+    public JSONArray hospitalAndAvailableLocation(@PathVariable String secialisationName){
+        return hospitalSpecialisationService.getAllInformationsBySpecialisation(secialisationName);
+    }
+
+
+
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+    @GetMapping("/book/{id}")
+    public JSONArray hospitalBook(@PathVariable Long id){
+        return hospitalSpecialisationService.bookHospital(id);
+    }
+
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+    @GetMapping("/CancelBook/{id}")
+    public JSONArray hospitalBookCancel(@PathVariable Long id){
+        return hospitalSpecialisationService.CancelBookHospital(id);
+    }
+
+
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+    @GetMapping("/allHospitals")
+    public List<Hospital> getAllHospital(){
+        return hospitalService.getAllHospital();
+    }
+
+
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+    @GetMapping("/allSpecialisations")
+    public List<Specialisation> getAllSpecialisation(){
+        return specialisationService.getAllSpecialisation();
+    }
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+    @GetMapping("/Specialisation/{name}")
+    public ResponseEntity<?>  findBySpecialisationName(@PathVariable String name) {
+
+        Specialisation specialisation =  specialisationService.getSpecialisationByName(name);
+        if(specialisation == null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }else{
+            return ResponseEntity.ok(specialisation);
+        }
+    }
+
 }
---
+++ appGestion/src/main/java/com/medhead/appGestion/jwt/JwtUtils.java
@@ -0,0 +1,55 @@
+package com.medhead.appGestion.jwt;
+
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+
+
+@Component
+public class JwtUtils {
+    private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
+
+    @Value("${bezkoder.app.jwtSecret}")
+    private String jwtSecret;
+
+    @Value("${bezkoder.app.jwtExpirationMs}")
+    private int jwtExpirationMs;
+
+    private Key key() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+
+    public boolean validateJwtToken(String authToken) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key()).build().parse(authToken);
+            return true;
+        } catch (MalformedJwtException e) {
+            logger.error("Invalid JWT token: {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            logger.error("JWT token is expired: {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            logger.error("JWT token is unsupported: {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            logger.error("JWT claims string is empty: {}", e.getMessage());
+        } catch (Exception r){
+            logger.error("all except: {}", r.getMessage());
+        }
+
+        return false;
+    }
+
+    public String getUserNameFromJwtToken(String token) {
+        return Jwts.parserBuilder().setSigningKey(key()).build()
+                .parseClaimsJws(token).getBody().getSubject();
+    }
+}
