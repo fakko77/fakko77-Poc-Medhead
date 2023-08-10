35075d2bb11af90beb02865410a2ec9a631e3f16
Morgan_Facorat <facorat.morgan@gmail.com>
2023-08-10 18:53:28 +0200

    Initialisation du microservice de gestion

---
+++ appGestion/src/main/java/com/medhead/appGestion/AppGestionApplication.java
@@ -0,0 +1,13 @@
+package com.medhead.appGestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppGestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppGestionApplication.class, args);
+	}
+
+}
---
+++ appGestion/src/test/java/com/medhead/appGestion/AppGestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medhead.appGestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppGestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
