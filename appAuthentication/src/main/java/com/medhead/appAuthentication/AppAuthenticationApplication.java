168c12249a69d76961660ca7eed0baeac582e8a7
Morgan_Facorat <facorat.morgan@gmail.com>
2023-08-10 18:02:55 +0200

    Initialisation du microservice d'authentification

---
+++ appAuthentication/src/main/java/com/medhead/appAuthentication/AppAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.medhead.appAuthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppAuthenticationApplication.class, args);
+	}
+
+}
